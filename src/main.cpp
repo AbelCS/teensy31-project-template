204b19beeaedc5182ff9a7780484fbdab6df4e97
Abel <belipowa@gmail.com>
2014-07-11 21:02:17 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "WProgram.h"
+
+extern "C" int main(void)
+{
+	pinMode(13, OUTPUT);
+
+	while (1) {
+		digitalWriteFast(13, HIGH);
+		delay(1000);
+		digitalWriteFast(13, LOW);
+		delay(1000);
+	}
+}
+
